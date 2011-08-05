a949ecb758b5a1201654816e13b3c08cc3278ff0
Cass <kasull88@gmail.com>
2011-08-05 15:12:16 -0400

    Working towards improved physics stuff

--- guffpong.c
+++ guffpong.c
@@ -22,6 +22,8 @@
 #define AI_FUDGE      0.5
 #define P1_X          370
 #define P2_X          (WINDOW_HEIGHT - P1_X - PADDLE_WIDTH)
+#define P_Y           (WINDOW_HEIGHT / 2 - PADDLE_HEIGHT)
+#define NUM_BODIES    7
 
 struct {
     bool up_down;
@@ -30,37 +32,62 @@ struct {
     bool right_down;
 } key_state = {false, false, false, false};
 
-typedef struct {
-    float vel_x, vel_y;
-    float x, y;
-    float radius;
-    float mass;
-    SDL_Surface *surface;
-} ball_t;
+enum body_index {
+    BODY_INDEX_WALL_TOP,
+    BODY_INDEX_WALL_BOTTOM,
+    BODY_INDEX_WALL_LEFT,
+    BODY_INDEX_WALL_RIGHT,
+    BODY_INDEX_P1,
+    BODY_INDEX_P2,
+    BODY_INDEX_BALL
+};
+
+typedef enum {
+    BODY_TYPE_HLINE,
+    BODY_TYPE_VLINE,
+    BODY_TYPE_BOX
+} body_type_t;
 
 typedef struct {
+    body_type_t type;
     float x, y;
-    float vel_x, vel_y;
+    float w, h;
+    float vx, vy;
     float mass;
-} paddle_t;
+} body_t;
+
+typedef struct {
+    float radius;
+    SDL_Surface *surface;
+    body_t *body;
+} ball_t;
 
 struct {
     unsigned int p1;
     unsigned int p2;
 } scores = {0, 0};
 
-ball_t ball;
-paddle_t p1 = {P1_X, (WINDOW_HEIGHT - PADDLE_HEIGHT) / 2, 0, 0, PADDLE_MASS};
-paddle_t p2 = {P2_X, (WINDOW_HEIGHT - PADDLE_HEIGHT) / 2, 0, 0, PADDLE_MASS};
+SDL_Surface *ball_surface;
+
+body_t ball;
+body_t p1 = {BODY_TYPE_BOX, P1_X, P_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 0, 0,
+             PADDLE_MASS};
+body_t p2 = {BODY_TYPE_BOX, P2_X, P_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 0, 0,
+             PADDLE_MASS};
+             
 SDLPango_Context *scores_context;
+
 int update_rects_n = 0;
 SDL_Rect *update_rects = NULL;
+
+body_t bodies[NUM_BODIES];
 
 bool running;
 
 void add_update(int x, int y, int w, int h) {
     update_rects = realloc(update_rects, sizeof(SDL_Rect) * (update_rects_n + 1));
     SDL_Rect rect = {x, y, w, h};
+    
     update_rects[update_rects_n++] = rect;
 }
 
@@ -68,6 +95,38 @@ void clear_updates(void) {
     free(update_rects);
     update_rects = NULL;
     update_rects_n = 0;
+}
+
+body_t new_body(body_type_t type, float x, float y, float w, float h, float vx,
+                 float vy, float mass) {
+    body_t body = {type, x, y, w, h, vx, vy, mass};
+    return body;
+}
+
+void init_bodies(void) {
+    bodies[BODY_INDEX_WALL_TOP]    = new_body(BODY_TYPE_HLINE,
+                                              0, 0,
+                                              WINDOW_WIDTH, 0,
+                                              0, 0,
+                                              0);
+    bodies[BODY_INDEX_WALL_BOTTOM] = new_body(BODY_TYPE_HLINE,
+                                              0, WINDOW_HEIGHT,
+                                              WINDOW_WIDTH, 0,
+                                              0, 0,
+                                              0);
+    bodies[BODY_INDEX_WALL_LEFT]   = new_body(BODY_TYPE_VLINE,
+                                              0, 0,
+                                              0, WINDOW_HEIGHT,
+                                              0, 0,
+                                              0);
+    bodies[BODY_INDEX_WALL_RIGHT]  = new_body(BODY_TYPE_VLINE,
+                                              WINDOW_WIDTH, 0,
+                                              0, WINDOW_HEIGHT,
+                                              0, 0,
+                                              0);
+    bodies[BODY_INDEX_P1]          = p1;
+    bodies[BODY_INDEX_P2]          = p2;
+    bodies[BODY_INDEX_BALL]        = ball;
 }
 
 void get_input(void) {
@@ -142,8 +201,8 @@ void draw_frame(SDL_Surface *screen) {
     // player 2's paddle
     
     // draw the circle
-    SDL_Rect ball_rect = {ball.x - 10, ball.y - 10, 20, 20};
-    SDL_BlitSurface(ball.surface, NULL, screen, &ball_rect);
+    SDL_Rect ball_rect = {ball.x, ball.y, 20, 20};
+    SDL_BlitSurface(ball_surface, NULL, screen, &ball_rect);
     // score text
     char text[9];
     snprintf(text, 9, "%03i  %03i", scores.p2, scores.p1);
@@ -158,14 +217,15 @@ void draw_frame(SDL_Surface *screen) {
 
 void reset_ball(void) {
     float mul = rand() / (float) RAND_MAX;
-    ball.vel_x = MAX(mul * 10, 10 - mul * 10);
-    ball.vel_y = 10 - ball.vel_x;
-    ball.x = WINDOW_WIDTH / 2;
-    ball.y = WINDOW_HEIGHT / 2;
-    ball.radius = BALL_RADIUS;
+    ball.vx = MAX(mul * 10, 10 - mul * 10);
+    ball.vy = 10 - ball.vx;
+    ball.x = WINDOW_WIDTH / 2 - BALL_RADIUS;
+    ball.y = WINDOW_HEIGHT / 2 - BALL_RADIUS;
+    ball.w = 2 * BALL_RADIUS;
+    ball.h = 2 * BALL_RADIUS;
     ball.mass = BALL_MASS;
     
-    ball.surface = IMG_Load("data/ball.png");
+    ball_surface = IMG_Load("data/ball.png");
 }
 
 void collide(float m0, float *u0, float m1, float *u1) {
@@ -175,78 +235,79 @@ void collide(float m0, float *u0, float m1, float *u1) {
     *u1 = v1;
 }
 
+void compute_collisions(void) {
+    
+}
+
 void ball_compute_position(void) {
-    float pos_int_x = ball.x + ball.vel_x;
-    float pos_int_y = ball.y + ball.vel_y;
+    float pos_int_x = ball.x + ball.vx;
+    float pos_int_y = ball.y + ball.vy;
     
     // top and bottom walls
-    if (pos_int_y <= ball.radius || pos_int_y >= WINDOW_HEIGHT - ball.radius)
-        ball.vel_y = -ball.vel_y;
+    if (pos_int_y <= 0 || pos_int_y + ball.h >= WINDOW_HEIGHT)
+        ball.vy = -ball.vy;
     // p1's paddle
     if (pos_int_y > p1.y && pos_int_y < p1.y + PADDLE_HEIGHT &&
-        (pos_int_x + ball.radius) >= p1.x &&
-        (pos_int_x + ball.radius) <= p1.x + PADDLE_WIDTH) {
-            collide(ball.mass, &ball.vel_x, p1.mass, &p1.vel_x);
-            ball.vel_y += p1.vel_y / 4;
+        (pos_int_x + ball.w) >= p1.x &&
+        (pos_int_x + ball.w) <= p1.x + PADDLE_WIDTH) {
+            collide(ball.mass, &ball.vx, p1.mass, &p1.vx);
+            ball.vy += p1.vy / 4;
     }
     // p2's paddle
     if (pos_int_y >= p2.y && pos_int_y <= p2.y + PADDLE_HEIGHT &&
-        (pos_int_x - ball.radius) <= p2.x + PADDLE_WIDTH &&
-        (pos_int_x - ball.radius) >= p2.x) {
-            collide(ball.mass, &ball.vel_x, p2.mass, &p2.vel_x);
-            ball.vel_y += p2.vel_y / 4;
+        pos_int_x <= p2.x + PADDLE_WIDTH && pos_int_x >= p2.x) {
+            collide(ball.mass, &ball.vx, p2.mass, &p2.vx);
+            ball.vy += p2.vy / 4;
     }
     
-    add_update(ball.x - ball.radius, ball.y - ball.radius, 2 * ball.radius,
-               2 * ball.radius);
+    add_update(ball.x, ball.y, ball.w, ball.h);
 
-    if (pos_int_x <= ball.radius) { // left wall
+    if (pos_int_x <= 0) { // left wall
         scores.p1++, reset_ball();
     }
-    else if (pos_int_x >= WINDOW_WIDTH - ball.radius) // right wall
+    else if (pos_int_x + ball.w >= WINDOW_WIDTH) // right wall
         scores.p2++, reset_ball();
     
     
-    ball.x += ball.vel_x;
-    ball.y += ball.vel_y;
+    ball.x += ball.vx;
+    ball.y += ball.vy;
     
-    add_update(ball.x - ball.radius, ball.y - ball.radius, 2 * ball.radius ,
-               2 * ball.radius);
+    add_update(ball.x, ball.y, ball.w, ball.h);
 }
 
-void paddle_move(paddle_t *paddle) {
+void paddle_move(body_t *paddle) {
     // use the current position of the paddle to determine which box it's in
     float paddle_x = paddle->x, paddle_y = paddle->y;
     
     // factor in friction for the paddle
-    paddle->vel_y = copysign(MAX(abs(paddle->vel_y) - 0.5, 0), paddle->vel_y);
-    paddle->y += paddle->vel_y;
+    paddle->vy = copysign(MAX(abs(paddle->vy) - 0.5, 0), paddle->vy);
+    paddle->y += paddle->vy;
     
     if (paddle->y > WINDOW_HEIGHT - PADDLE_HEIGHT) {
         paddle->y = WINDOW_HEIGHT - PADDLE_HEIGHT;
-        paddle->vel_y = -paddle->vel_y;
+        paddle->vy = -paddle->vy;
     } else if (paddle->y < 0) {
         paddle->y = 0;
-        paddle->vel_y = -paddle->vel_y;
+        paddle->vy = -paddle->vy;
     }
     
-    paddle->vel_x = copysign(MAX(abs(paddle->vel_x) - 0.5, 0), paddle->vel_x);
-    paddle->x += paddle->vel_x;
+    paddle->vx = copysign(MAX(abs(paddle->vx) - 0.5, 0), paddle->vx);
+    paddle->x += paddle->vx;
     if (paddle_x >= WINDOW_WIDTH / 2) {
         if (paddle->x > WINDOW_WIDTH - PADDLE_WIDTH) {
             paddle->x = WINDOW_WIDTH - PADDLE_WIDTH;
-            paddle->vel_x = -paddle->vel_x;
+            paddle->vx = -paddle->vx;
         } else if (paddle->x < WINDOW_WIDTH / 2) {
             paddle->x = WINDOW_WIDTH / 2;
-            paddle->vel_x = -paddle->vel_x;
+            paddle->vx = -paddle->vx;
         }
     } else {
         if (paddle->x < 0) {
             paddle->x = 0;
-            paddle->vel_x = -paddle->vel_x;
+            paddle->vx = -paddle->vx;
         } else if (paddle->x > WINDOW_WIDTH / 2 - PADDLE_WIDTH) {
             paddle->x = WINDOW_WIDTH / 2 - PADDLE_WIDTH;
-            paddle->vel_x = -paddle->vel_x;
+            paddle->vx = -paddle->vx;
         }
     }
     
@@ -261,9 +322,9 @@ void paddle_move(paddle_t *paddle) {
 
 void ai_paddle(void) {
     if (p2.y + PADDLE_HEIGHT / 2 > ball.y + AI_FUDGE)
-        p2.vel_y -= 2;
+        p2.vy -= 2;
     else if (p2.y + PADDLE_HEIGHT / 2 < ball.y - AI_FUDGE)
-        p2.vel_y += 2;
+        p2.vy += 2;
 }
 
 void game_loop(SDL_Surface *screen) {
@@ -272,6 +333,8 @@ void game_loop(SDL_Surface *screen) {
     srand(time(NULL));
     
     reset_ball();
+    
+    init_bodies();
     
     scores_context = SDLPango_CreateContext_GivenFontDesc("Sans 20");
     SDLPango_SetDefaultColor(scores_context,
@@ -284,13 +347,13 @@ void game_loop(SDL_Surface *screen) {
         get_input();
 
         if (key_state.up_down)
-            p1.vel_y -= 2;
+            p1.vy -= 2;
         if (key_state.down_down)
-            p1.vel_y += 2;
+            p1.vy += 2;
         if (key_state.left_down)
-            p1.vel_x -= 2;
+            p1.vx -= 2;
         if (key_state.right_down)
-            p1.vel_x += 2;
+            p1.vx += 2;
         
         ai_paddle();
         
